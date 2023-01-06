bec6f15778c6e1b299797cd8ffd11c632afa876d
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2023-01-06 13:16:37 +0200

    2. Zoo - completed.

---
+++ Inheritance - Ex/2. Zoo/Zoo/src/zoo/Main.java
@@ -0,0 +1,8 @@
+package zoo;
+
+public class Main {
+    public static void main(String[] args) {
+        Lizard lizard = new Lizard("Vladi");
+        System.out.println(lizard.getName());
+    }
+}
